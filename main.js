2632557e45d804d42b62e2502edd2ecf0f7dfe3d
GalaxyJavaScript <105827309+GalaxyJavaScript@users.noreply.github.com>
2023-01-18 18:03:47 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-https://teachablemachine.withgoogle.com/models/l8wlL0kAP/
+
+
+function startClassification(){
+    navigator.mediaDevices.getUserMedia({ audio:true});
+    classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/l8wlL0kAP/model.json', modelReady)
+}
+
+function modelReady(){
+    classifier.classify(gotResults);
+}
